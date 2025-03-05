43616431293cfd041dc96019de9ec154165f1827
rati234 <bkhandamashvili@gmail.com>
2025-03-05 16:43:52 +0400

    commit

--- day 05/classwork/lesson.py
+++ day 05/classwork/lesson.py
@@ -1,2 +1,7 @@
 num1 = int(input("please enter first number: "))
-num2 = int(input("please enter second number: ")) 
+num2 = int(input("please enter second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2) 
+print(num1 / num2) 
