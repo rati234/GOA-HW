0db42ac62f1f02321466829841ce5a6dffc655a4
rati234 <bkhandamashvili@gmail.com>
2025-03-06 20:33:35 +0400

    commit

---
+++ day 05/homework/.lesson.py
@@ -0,0 +1,67 @@
+password = "Dexter" 
+
+
+  
+     
+passwordd = (input("please enter the password")) 
+ 
+if password ==passwordd:  
+    print("granted") 
+else: 
+    print("deined") 
+
+
+
+#მე-3 დავალება
+
+
+
+number =int(input("please enter current number"))
+
+if number % 2 == 0:
+    print("The number is even") 
+else:
+    print("The number is odd")
+
+
+#მე-4 დავალება
+
+
+numbers = [5,3,5,4, 5] 
+
+for i in range(5):
+
+    num = float(input(f"enter number {i+1}: ")) 
+    numbers.append 
+
+average = sum(numbers) / len(numbers) 
+
+print(f"The average of entered numbers is: {average:.2f}") 
+
+#მე-5 დავალება 
+
+celsius = float(input("enter the temperature in celsius")) 
+
+fahrenheit = (celsius * 9/5) + 32 
+
+print(f"the temperature in fahrenheit is: {fahrenheit:.2f} 25") 
+    
+
+#მე-6 დავალება
+
+
+
+fahrenheit = float(input("please enter the temperature in farhenheit")) 
+
+celsius = (fahrenheit - 32) * 5/9 
+
+
+print(f"the temperature in celsius is: {celsius:.2} 35") D
+
+
+
+
+
+
+
+
